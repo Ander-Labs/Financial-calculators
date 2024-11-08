f9dfa3d492c1997c4c5d44b6038a7f95cbd46122
andersonruizr2 <andersonruizr2@gmail.com>
2024-11-08 22:25:04 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-//import sentry from "@sentry/astro";
+import sentry from "@sentry/astro";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -14,21 +14,21 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    /* sentry({
+    sentry({
       dsn: "https://f7a72e2a5eb8a2d45158781a3f87b715@o4508263619035136.ingest.us.sentry.io/4508263632011264",
       environment: "production",
       sourceMapsUploadOptions: {
         project: "financial-calculators",
         authToken: process.env.SENTRY_AUTH_TOKEN,
       },
-    }),*/
+    }),
     tailwind(),
     sitemap(),
     partytown({
       // Example: Disable debug mode.
       config: {
         debug: true,
-        // forward: ["dataLayer.push"],
+        forward: ["dataLayer.push"],
       },
     }),
     icon(),
