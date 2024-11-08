be891ea14aaccd0ff69fc44e0fd5bc824d995049
andersonruizr2 <andersonruizr2@gmail.com>
2024-11-08 17:35:43 +0000

    update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,16 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), tailwind(), sitemap(), partytown()]
+  integrations: [
+    sentry({
+      dsn: "https://f7a72e2a5eb8a2d45158781a3f87b715@o4508263619035136.ingest.us.sentry.io/4508263632011264",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    tailwind(),
+    sitemap(),
+    partytown(),
+  ],
 });
