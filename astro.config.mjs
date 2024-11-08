751b095df21e748a19680a795eee6a146c954443
ANDER <andersonruizr2@gmail.com>
2024-11-08 12:16:23 -0500

    update file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sentry from '@sentry/astro';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sentry(), tailwind(), sitemap(), partytown()]
+});
