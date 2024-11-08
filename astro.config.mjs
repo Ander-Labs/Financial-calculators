1f05d635f81efb55838f3b712a1a096e91af45bc
andersonruizr2 <andersonruizr2@gmail.com>
2024-11-08 22:30:05 +0000

    update file

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,14 @@ import partytown from "@astrojs/partytown";
 
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sentry({
       dsn: "https://f7a72e2a5eb8a2d45158781a3f87b715@o4508263619035136.ingest.us.sentry.io/4508263632011264",
-      environment: "production",
+      // environment: "production",
       sourceMapsUploadOptions: {
         project: "financial-calculators",
         authToken: process.env.SENTRY_AUTH_TOKEN,
@@ -33,4 +35,11 @@ export default defineConfig({
     }),
     icon(),
   ],
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
